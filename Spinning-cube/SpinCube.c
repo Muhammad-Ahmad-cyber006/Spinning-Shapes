2823f9c4c77322806ea80a8a63fef7bf7a791f4b
Muhammad-Ahmad-cyber006 <novahat2580@gmail.com>
2026-06-10 23:31:11 +0500

    cube is spinning screen is added for better printing

--- Spinning-cube/SpinCube.c
+++ Spinning-cube/SpinCube.c
@@ -5,54 +5,58 @@
 
  float A,B,C;
  float cubeWidth=10;
- int width=70;
+ int width=67;
  int height=55;
- float zBuffer[70*55];
- char buffer[70*55];
+ float zBuffer[67*55];
+ char buffer[67*150];
+ char screen[(67*150)+55+1];
  int backgroundACSIICODE=' ';
  float horizontalOffset;
 
- float incermentSpeed=0.8;
-int idx;
+ float incermentSpeed=0.7; 
+ int idx;
  int distanceFromCam =60;
- float K1=40;
+ float K1=28; 
  float x,y,z;
  float ooz;
  int xp,yp;
  
- float calculateX(int i, int j, int k)
+ float calculateX(float i, float j, float k)
  {
      return j*sin(A)*sin(B)*cos(C)
      - k*cos(A)*sin(B)*cos(C)
-     + j * cos(A)*sin(C) + k * sin(A)*sin(C) 
+     + j * cos(A)*sin(C) + k * sin(A)*sin(C)
      + i *cos(B) *cos(C);
      
-    }
+ }
     
-float calculateY(int i,int j ,int k)
+float calculateY(float i,float j ,float k)
 {
     return j*cos(A)*cos(C) + k *sin(A) * cos(C) - j *sin(A)*sin(B)*sin(C)
-          +k*cos(A)*sin(B)*sin(C) - i*cos(B)*sin(C);  
+          +k*cos(A)*sin(B)*sin(C) - i*cos(B)*sin(C);
 }
 
-float calculateZ(int i, int j ,int k)
+float calculateZ(float i, float j ,float k)
 {
     return k*cos(A)*cos(B) - j*sin(A)*cos(B)
         +i *sin(B);
 }
+
  void calculateForSurface(float cubeX,float cubeY,float cubeZ, int ch)
  {
     x= calculateX(cubeX,cubeY,cubeZ);
     y= calculateY(cubeX,cubeY,cubeZ);
     z= calculateZ(cubeX,cubeY,cubeZ)+ distanceFromCam;
 
-    ooz=1/z;
-    
-    xp=(int)(width/2+ horizontalOffset+K1*ooz*x*2);
+    ooz=1.0f/z;
+
+    xp=(int)(width/2+horizontalOffset+K1*ooz*x*2);
     yp=(int)(height/2+K1*ooz*y);
-  idx =xp+yp*width;
-    if(idx>=0&&idx<width*height)
+
+    if(xp>=0&&xp<width&&yp>=0&&yp<height)
     {
+        idx=xp+yp*width;
+
         if(ooz>zBuffer[idx])
         {
             zBuffer[idx]=ooz;
@@ -60,41 +64,69 @@ float calculateZ(int i, int j ,int k)
         }
 
     }
-    
 
 }
 
 int main()
 {
     printf("\x1b[2J");
+    printf("\x1b[?25l");
+
     while(1)
     {
-        memset(buffer,backgroundACSIICODE,width*height);
-        memset(zBuffer,0,width*height*4);
-        horizontalOffset = 1 * cubeWidth;
+        memset(buffer,backgroundACSIICODE,sizeof(buffer));
+        memset(zBuffer,0,sizeof(zBuffer));
+
+        horizontalOffset=18;
+
         for(float cubeX=-cubeWidth;cubeX<cubeWidth;cubeX+=incermentSpeed)
         {
-            for(float cubeY= - cubeWidth;cubeY<cubeWidth;cubeY+=incermentSpeed)
+            for(float cubeY=-cubeWidth;cubeY<cubeWidth;cubeY+=incermentSpeed)
             {
-                        calculateForSurface(cubeX, cubeY, -cubeWidth, '.');
-                        calculateForSurface(cubeWidth, cubeY, cubeX, '&');
-                        calculateForSurface(-cubeWidth, cubeY, -cubeX, '$');
-                        calculateForSurface(-cubeX, cubeY, cubeWidth, '*');
-                        calculateForSurface(cubeX, -cubeWidth, -cubeY, '@');
-                        calculateForSurface(cubeX, cubeWidth, cubeY, '#');
+                // Front face
+                calculateForSurface(cubeX,cubeY,-cubeWidth,'.');
+
+                // Right face
+                calculateForSurface(cubeWidth,cubeY,cubeX,'$');
+
+                // Left face
+                calculateForSurface(-cubeWidth,cubeY,-cubeX,'#');
+
+                // Back face
+                calculateForSurface(-cubeX,cubeY,cubeWidth,'@');
+
+                // Bottom face
+                calculateForSurface(cubeX,-cubeWidth,-cubeY,'+');
+
+                // Top face
+                calculateForSurface(cubeX,cubeWidth,cubeY,'&');
             }
         }
-        printf("\x1b[H");
-        for(int k=0;k<width*height;k++)
+
+        int p=0;
+        for(int row=0;row<height;row++)
         {
-            putchar(k%width ? buffer[k]:10);
+            for(int col=0;col<width;col++)
+            {
+                screen[p++]=buffer[col+row*width];
+            }
 
+            screen[p++]='\n';
         }
-        A+=0.05;
-        B+=0.05;
-        C=0.01;
-        usleep(16000);
 
+        screen[p]='\0';
+   
+        printf("\x1b[H%s",screen);
+        fflush(stdout);
+
+        A+=0.03;
+        B+=0.03;
+        C+=0.01;
+
+        usleep(16000);
     }
+
+    printf("\x1b[?25h");
+
     return 0;
 }
